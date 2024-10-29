0ba576c7a19b775b05c98dc3ec5b0e2e67a9bdd5
Apurva <apurvaapr132gmail.com>
2024-10-29 19:29:37 +0530

    my account page created

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
-<<<<<<< HEAD
 session_start(); // This should be at the very top of your index.php file
 include('C:\Users\apurv\resincustomisedproducts\includes\connect.php');
 include('C:\Users\apurv\resincustomisedproducts\commonfunctions.php');
-=======
 include('./includes/connect.php');
 include('./commonfunctions.php');
->>>>>>> 7d515472d15ba7545687fc108444011f7c3cee99
 ?>
 
 
